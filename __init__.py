f4de3d1eac942f1bb8e9ff3e214e0722bb3f9595
flothesof <florian.s.lebourdais@gmail.com>
2023-11-08 11:26:32 +0100

    update call to resources by reading local folder

--- __init__.py
+++ __init__.py
@@ -11,12 +11,14 @@ from .activity import Activity
 from .config import hmd
 from .links import initializeLinks
 
+# load resources from folder
 current_file_path = os.path.dirname(os.path.abspath(__file__))
-svg_left_arrow = """<svg height="10px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" class="svg-inline--fa fa-chevron-left fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg>"""
-svg_right_arrow = """<svg height="10px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg>"""
-js_bytes = open(current_file_path + '/resources/more_heatmap.js', 'rb').read().decode()
-css_bytes = open(current_file_path + '/resources/more_heatmap.css', 'rb').read().decode()
+svg_left_arrow_str = open(current_file_path + '/resources/left_arrow.svg', 'r').read()
+svg_right_arrow_str = open(current_file_path + '/resources/right_arrow.svg', 'r').read()
+js_str = open(current_file_path + '/resources/more_heatmap.js', 'rb').read().decode()
+css_str = open(current_file_path + '/resources/more_heatmap.css', 'rb').read().decode()
 
+# css that will get inserted in the HTML template
 css = """<style> 
 
     .jumbotron {
@@ -116,7 +118,7 @@ css = """<style>
 
 </style>"""
 
-html_heatmap = """
+html_heatmap_template = """
 
     <script>
     var pybridge = function(arg){{{{
@@ -199,7 +201,7 @@ def deckbrowserRenderStats():
     config = mw.addonManager.getConfig(__name__)
 
     # format the heatmap by inserting the relevant information in the HTML
-    formatted_heatmap = html_heatmap.format(
+    formatted_heatmap = html_heatmap_template.format(
         css=css,
         data=json.dumps(dict(everything['data'])),
         legend=json.dumps(everything['legend']),
@@ -215,10 +217,10 @@ def deckbrowserRenderStats():
         heatmap_cell_font=config['heatmap-cell-font'],
         heatmap_cell_weight=config['heatmap-cell-weight'],
         heatmap_margin_top=config['heatmap-margin-top'],
-        svg_left_arrow=svg_left_arrow,
-        svg_right_arrow=svg_right_arrow,
-        js_bytes=js_bytes,
-        css_heatmap=css_bytes,
+        svg_left_arrow=svg_left_arrow_str,
+        svg_right_arrow=svg_right_arrow_str,
+        js_bytes=js_str,
+        css_heatmap=css_str,
     )
 
     return formatted_heatmap
